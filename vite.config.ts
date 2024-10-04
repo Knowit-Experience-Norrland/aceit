6a59dbeafa70770d7581806fea6a16209d6ea7ee
linulas <linubran90@gmail.com>
2024-10-04 13:52:23 +0200

    bugfix sass error

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
 	css: {
 		preprocessorOptions: {
 			scss: {
-				additionalData : `@import "$lib/style/theme.scss"`
+				additionalData : `@import "$lib/style/theme.scss";`
 			}
 		}
 	}
