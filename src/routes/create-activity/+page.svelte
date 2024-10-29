d2330b78e8df6e77dea2b5817fac7c48822752c1
linulas <linubran90@gmail.com>
2024-10-29 16:38:23 +0100

    ads basic style to /create-activity

--- src/routes/create-activity/+page.svelte
+++ src/routes/create-activity/+page.svelte
@@ -1,26 +1,62 @@
 <script lang="ts">
+	import Input from '$lib/components/input.svelte';
 	import type { ActionData, PageServerData } from './$types';
 
 	export let data: PageServerData;
 	export let form: ActionData;
 </script>
 
-<h1>Add Activity</h1>
+<h1>Skapa aktivitet</h1>
 
 <form action="" method="POST">
-	<label for="name_input">Name</label>
-	<input id="name_input" name="name" type="text" value={form?.name || ''} />
-	<label for="holes_input">Holes</label>
-	<input id="holes_input" name="holes" type="number" value={form?.holes || 0} />
+	<Input label="Namn på aktivitet" id="name_input" name="name" value={form?.name || ''} />
+	<Input label="Antal hål" id="holes_input" name="holes" type="number" value={form?.holes || 0} />
+	<Input
+		label="Beskrivning"
+		id="description_input"
+		name="description"
+		value={form?.description || ''}
+	/>
 
-	{#each data.users as user}
-		<label for={user.id}>{user.firstName} {user.lastName}</label>
-		<input type="checkbox" name="users" value={user.id} id={user.id} />
-	{/each}
+	<fieldset>
+		<legend>Lägg till deltagare</legend>
+		{#each data.users as user}
+			<span>
+				<input type="checkbox" name="users" value={user.id} id={user.id} />
+				<label for={user.id}>{user.firstName} {user.lastName}</label>
+			</span>
+		{/each}
+	</fieldset>
 
 	<button>Add</button>
 </form>
 
 {#if form?.error}
-  <p class="error">{form.error}</p>
+	<p class="error">{form.error}</p>
 {/if}
+
+<style lang="scss">
+	h1 {
+		@include text-lg;
+		margin: $m-lg;
+	}
+
+	form {
+		display: grid;
+		gap: 1rem;
+	}
+
+	fieldset {
+		display: flex;
+		flex-direction: column;
+	}
+
+	input {
+		padding: 0.5rem;
+	}
+
+  button {
+    flex: 1;
+    min-width: 0;
+  }
+</style>
