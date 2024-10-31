6285ae7d10f50a845dddca30a7895fe4e8e546af
Henrik Pettersson <henrik.pettersson@knowit.se>
2024-10-31 08:52:59 +0100

    add profile page

---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	let test = 'hejsan';
+
+	function updateButton() {
+		test = 'hejdå';
+	}
+</script>
+
+<h1>Profilsida</h1>
+
+<div class="bg-box">
+	<p>Namn Namnsson</p>
+	<p>namn.namnsson@knowit.se</p>
+</div>
+
+
+
+<style lang="scss">
+	.bg-box {
+		background-color: blue;
+		padding: 1.5rem 2rem;
+	}
+</style>
