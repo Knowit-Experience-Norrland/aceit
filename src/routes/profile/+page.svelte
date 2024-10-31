fd7ce1a06b0381fcb5aabc2461ca025a839a025d
Henrik Pettersson <henrik.pettersson@knowit.se>
2024-10-31 12:25:56 +0100

    add color to icon

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,16 +1,19 @@
 <script lang="ts">
-	import Icon from '$lib/components/icon_star.svelte';
+	import IconStar from '$lib/components/icon_star.svelte';
 </script>
 
 <h1>Profilsida</h1>
 
 <div class="bg-box">
-	<Icon width="32" height="32" fill="green" />
+	<IconStar width="32" height="32" fill="var(--knowit-green)" />
+
 	<h2>Namn Namnsson</h2>
 	<p>namn.namnsson@knowit.se</p>
 	<a href="/target-page">Logga ut</a>
 </div>
 
 <style lang="scss">
-
+	:root {
+		--knowit-green: #{$knowit-green};
+	}
 </style>
