eacf0dfe3fe4b6bac163b8173e3e566dd0534c5f
Henrik Pettersson <henrik.pettersson@knowit.se>
2024-10-31 12:37:55 +0100

    add svg icon components

---
+++ src/lib/components/icon_chevron_left.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let width  = '9';
+	export let height = '16';
+	export let fill   = 'currentColor';
+</script>
+
+<svg
+	width={width}
+	height={height}
+	viewBox="0 0 9 16"
+	xmlns="http://www.w3.org/2000/svg"
+	fill={fill}
+	aria-label="icon"
+>
+	<path d="M2.77701 8.00003L8.91034 14.1334L7.96667 15.077L0.889672 8.00003L7.96667 0.923026L8.91034 1.86669L2.77701 8.00003Z"/>
+</svg>
