11424e69766e3c8655f736e19e9ac0d1e4d32535
Henrik Pettersson <henrik.pettersson@knowit.se>
2024-10-31 09:43:40 +0100

    add svg icon to components

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,16 +1,14 @@
 <script lang="ts">
-	let test = 'hejsan';
-
-	function updateButton() {
-		test = 'hejdå';
-	}
+	import Icon from '$lib/components/icon.svelte';
 </script>
 
 <h1>Profilsida</h1>
 
 <div class="bg-box">
-	<p>Namn Namnsson</p>
+	<Icon width="32" height="32" fill="green" />
+	<h2>Namn Namnsson</h2>
 	<p>namn.namnsson@knowit.se</p>
+	<a href="/target-page">Logga ut</a>
 </div>
 
 
