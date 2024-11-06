ae85857ab6bb76ac0819c5e3230326b157caca19
Henrik Pettersson <henrik.pettersson@knowit.se>
2024-11-06 09:05:53 +0100

    knapp

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -3,27 +3,27 @@ import { login, cookieOptions } from "$lib/server/authenticate";
 import type { Actions } from "./$types";
 
 export const actions: Actions = {
-  default: async (event) => {
-    const data = await event.request.formData();
+	default: async (event) => {
+		const data = await event.request.formData();
 
-    const email = (data.get("email") as string)
-      ?.toLowerCase()
-      ?.trim();
-    const password = data.get("password") as string;
+		const email = (data.get("email") as string)
+		?.toLowerCase()
+		?.trim();
+		const password = data.get("password") as string;
 
-    const { error, token } = await login(email, password);
+		const { error, token } = await login(email, password);
 
-    if (error) {
-      return fail(400, { email, error });
-    }
+		if (error) {
+			return fail(400, { email, error });
+		}
 
-    if (!token) {
-      console.error("token was empty after login");
-      return fail(500, { email, error: "Internal server error" });
-    }
+		if (!token) {
+			console.error("token was empty after login");
+			return fail(500, { email, error: "Internal server error" });
+		}
 
-    event.cookies.set("token", token, cookieOptions);
+		event.cookies.set("token", token, cookieOptions);
 
-    throw redirect(302, "/");
-  }
+		throw redirect(302, "/");
+	}
 };
